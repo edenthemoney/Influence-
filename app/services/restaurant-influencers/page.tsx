2e4a733cabd591cd8d91cb935e8aead73567e0d7
edenthemoney <edenmusicllc@gmail.com>
2026-09-25 14:21:57 -0400

    Add restaurant influencer geo pages (hire influencers by city)

---
+++ app/services/restaurant-influencers/page.tsx
@@ -0,0 +1,7 @@
+import HouseCreatorsLanding, { houseMetadata } from '../../components/HouseCreatorsLanding';
+
+export const metadata = houseMetadata();
+
+export default function Page() {
+  return <HouseCreatorsLanding />;
+}
